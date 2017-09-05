ec3a3623df508bae8c542b374eebdf2d5b15bf4f
Alex Kondov <alexkondov38@gmail.com>
2017-09-05 17:24:19 +0300

    swipe navigation component style merging

--- Components/Elements/SwipeNavi.php
+++ Components/Elements/SwipeNavi.php
@@ -58,38 +58,37 @@ trait SwipeNavi {
     public function getComponentSwipeNavi(int $totalcount, int $currentitem, $color = 'white', $parameters=array(),array $styles=array()) {
         /** @var BootstrapView $this */
 
-            $count = 1;
-            $row = array();
+        $count = 1;
+        $row = array();
 
-            if($color == 'white'){
-                $color_normal = "#80ffffff";
-                $color_selected = "#ffffff";
+        if($color == 'white'){
+            $color_normal = "#80ffffff";
+            $color_selected = "#ffffff";
+        } else {
+            $color_normal = "#4D000000";
+            $color_selected = "#000000";
+        }
+
+        while($count <= $totalcount){
+
+            if($count == 1){
+                $margin = '0 0 0 0';
             } else {
-                $color_normal = "#4D000000";
-                $color_selected = "#000000";
+                $margin = '0 0 0 3';
             }
 
-            while($count <= $totalcount){
-
-                if($count == 1){
-                    $margin = '0 0 0 0';
-                } else {
-                    $margin = '0 0 0 3';
-                }
-
-                if($count == $currentitem){
-                    $row[] = $this->getComponentText('•',array(),array('color' => $color_selected,'font-size' => '27','width' => '14','text-align' => 'center','margin' => $margin));
-                } else {
-                    $row[] = $this->getComponentText('•',array(),array('color' => $color_normal,'font-size' => '27','width' => '14','text-align' => 'center','margin' => $margin));
-                }
-
-                $count++;
+            if($count == $currentitem){
+                $row[] = $this->getComponentText('•',array(),array('color' => $color_selected,'font-size' => '27','width' => '14','text-align' => 'center','margin' => $margin));
+            } else {
+                $row[] = $this->getComponentText('•',array(),array('color' => $color_normal,'font-size' => '27','width' => '14','text-align' => 'center','margin' => $margin));
             }
 
-            $obj = $this->getComponentRow($row,array(),array('width' => '100%', 'text-align' => 'center'));
-            $obj = $this->attachStyles($obj, $styles);
+            $count++;
+        }
 
-            return $obj;
-	}
+        $obj = $this->getComponentRow($row,array(),array_merge(array('width' => '100%', 'text-align' => 'center'), $styles));
+
+        return $obj;
+    }
 
 }
