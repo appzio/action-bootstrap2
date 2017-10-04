9d7e8d4f9bab12e72fc3beaa602f985e791a389b
Timo Railo <timo@appzio.com>
2017-10-04 11:05:10 +0300

    - documentation updates - fix for the html component

--- Components/ClientComponents/Html.php
+++ Components/ClientComponents/Html.php
@@ -6,7 +6,11 @@ use Bootstrap\Views\BootstrapView;
 trait Html {
 
     /**
-     * @param $content string, HTML formatted text. Note that this does not support all HTML notation
+     * @param $content string, HTML formatted text. Note that this does not support all HTML notation and this is
+     * one of the few exceptions where support differs between iOS and Android. On iOS only font size and color are
+     * supported whereas Android uses a webview to render the content. This is due to performance, where Android is
+     * much more performant on rendering html content. You can also apply normal appzio formatting for the entire text
+     * block.
      * @param array $parameters selected_state, variable, onclick, style
      * <code>
      * $array = array(
@@ -24,7 +28,7 @@ trait Html {
         /** @var BootstrapView $this */
 
 		$obj = new \StdClass;
-        $obj->type = 'msg-plain';
+        $obj->type = 'msg-html';
         $obj->content = $content;
 
         $obj = $this->attachStyles($obj,$styles);
--- Views/BootstrapView.php
+++ Views/BootstrapView.php
@@ -39,17 +39,17 @@ class BootstrapView extends BootstrapComponent implements BootstrapViewInterface
     public $components;
 
     /** Includes an array of colors defined for the app / branch / action
-     * <code>array ( [text_color] => #FF000000 [icon_color] => #FF000000 [background_color] => #FFCFD8DC [button_text] => #FF000000
-    [dark_button_text] => #FFFFFFFF [top_bar_text_color] => #FFFFFFFF [top_bar_icon_color] => #FFFFFFFF
-    [button_more_info_color] => #FF000000 [button_more_info_icon] => #FFFFFFFF [button_more_info_icon_color] => #FFFFFFFF
-    [button_more_info_text_color] => #FFFFFFFF [item_text_color] => #FFFFFFFF [top_bar_color] => #FFD32F2F
-    [button_color] => #FF536DFE [item_color] => #FFFFCDD2 [button_icon_color] => #FFFFFFFF
-    [button_text_color] => #FFFFFFFF [side_menu_color] => #FFFFFFFF [side_menu_text_color] => #FF000000 )</code>
+     * <code>array ( [text_color] => "#FF000000", [icon_color] => "#FF000000", [background_color] => "#FFCFD8DC", [button_text] => "#FF000000",
+    [dark_button_text] => "#FFFFFFFF", [top_bar_text_color] => "#FFFFFFFF", [top_bar_icon_color] => "#FFFFFFFF",
+    [button_more_info_color] => "#FF000000", [button_more_info_icon] => "#FFFFFFFF", [button_more_info_icon_color] => "#FFFFFFFF",
+    [button_more_info_text_color] => "#FFFFFFFF", [item_text_color] => "#FFFFFFFF", [top_bar_color] => "#FFD32F2F",
+    [button_color] => "#FF536DFE", [item_color] => "#FFFFCDD2", [button_icon_color] => "#FFFFFFFF",
+    [button_text_color] => "#FFFFFFFF", [side_menu_color] => "#FFFFFFFF", [side_menu_text_color] => "#FF000000" )</code>
      */
     public $colors;
 
     /**
-     * The current menu id if set
+     * The current menu id if set. Use $this->getMenuId() instead of accessing this property directly.
      *
      * @var
      */
