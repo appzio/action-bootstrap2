dcbb426b0e732af937f60fdb5ace0e6cb93c5c68
Alex Kondov <alexkondov38@gmail.com>
2017-12-06 15:10:13 +0200

    changes on ui kit people list

--- Components/AppzioUiKit/Listing/uiKitPeopleList.php
+++ Components/AppzioUiKit/Listing/uiKitPeopleList.php
@@ -6,17 +6,25 @@ use Bootstrap\Views\BootstrapView;
 trait uiKitPeopleList
 {
     /**
-     * @param array $content
+     * @param array $people
+     * array(
+     *     0 => array(
+     *         'name' - the name of the person - required,
+     *         'info' - additional information - required,
+     *         'contact' - contact information for the person - required,
+     *         'onclick' - action to be executed on row click - required
+     *     )
+     * )
      * @param array $parameters
      * @param array $styles
      * @return \stdClass
      */
-    public function uiKitPeopleList(array $content, array $parameters = array(), array $styles = array())
+    public function uiKitPeopleList(array $people, array $parameters = array(), array $styles = array())
     {
         /** @var BootstrapView $this */
         $items = array();
 
-        foreach ($content as $item) {
+        foreach ($people as $item) {
             $items[] = $this->getPersonRow($item);
             $items[] = $this->getComponentSpacer(1, array(), array(
                 'background-color' => '#fafafa'
@@ -28,29 +36,43 @@ trait uiKitPeopleList
 
     protected function getPersonRow($person)
     {
+        $divLayout = new \stdClass();
+        $divLayout->top = 80;
+        $divLayout->bottom = 0;
+        $divLayout->left = 0;
+        $divLayout->right = 0;
+
         /** @var BootstrapView $this */
         return $this->getComponentRow(array(
             $this->getComponentColumn(array(
                 $this->getComponentText($person['name'], array(), array(
                     'padding' => '0 0 0 0',
-                    'font-weight' => 'bold'
+                    'font-ios' => 'OpenSans',
+                    'font-size' => '14'
                 )),
-                $this->getComponentText($person['info'], array(), array(
+                $this->getComponentText(strtoupper($person['info']), array(), array(
                     'padding' => '0 0 0 0',
                     'color' => '#a8a8a8',
-                    'font-size' => '14'
+                    'font-size' => '12',
+                    'font-ios' => 'OpenSans'
                 )),
                 $this->getComponentText($person['contact'], array(), array(
                     'padding' => '0 0 0 0',
                     'color' => '#a8a8a8',
-                    'font-size' => '14'
+                    'font-size' => '12',
+                    'font-ios' => 'OpenSans'
                 ))
             ))
         ), array(
-            'onclick' => $person['onclick']
+            'onclick' => $this->getOnclickShowDiv('email', array(
+                'background' => 'blur',
+                'tap_to_close' => 1,
+                'transition' => 'from-bottom',
+                'layout' => $divLayout
+            ))
         ), array(
             'vertical-align' => 'middle',
-            'padding' => '15 0 15 0'
+            'padding' => '10 0 10 20',
         ));
     }
 }
