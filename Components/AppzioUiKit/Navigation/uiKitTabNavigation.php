c23f224fc7826c5d39be100cc7735d8405220da2
Alex Kondov <alexkondov38@gmail.com>
2018-01-10 12:16:51 +0200

    added disabled option on tab navigation component

--- Components/AppzioUiKit/Navigation/uiKitTabNavigation.php
+++ Components/AppzioUiKit/Navigation/uiKitTabNavigation.php
@@ -23,7 +23,20 @@ trait uiKitTabNavigation
     {
         /** @var BootstrapComponent $this */
 
-        if (!$tab['active']) {
+        if (isset($tab['disabled']) && $tab['disabled']) {
+            return $this->getComponentText($tab['text'], array(), array(
+                'color' => '#e3e1e1',
+                'padding' => '20 0 20 0',
+                'text-align' => 'center',
+                'background-color' => '#ffffff',
+                'border-width' => '1',
+                'border-color' => '#fafafa',
+                'font-size' => '14',
+                'width' => $this->screen_width / $count,
+                'font-ios' => 'OpenSans',
+                'font-android' => 'OpenSans'
+            ));
+        } else if (!$tab['active']) {
             return $this->getComponentText($tab['text'], array(
                 'onclick' => $tab['onclick'],
             ), array(
