8959552ee79550c82d24800df6aec5e9dc2e560c
Alex Kondov <alexkondov38@gmail.com>
2018-01-30 11:45:07 +0200

    gallery component scaffolding

---
+++ Components/ClientComponents/Gallery.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Bootstrap\Components\ClientComponents;
+use Bootstrap\Views\BootstrapView;
+
+trait Gallery {
+
+
+    public function getComponentGallery() {
+        /** @var BootstrapView $this */
+
+    }
+}
+
+
