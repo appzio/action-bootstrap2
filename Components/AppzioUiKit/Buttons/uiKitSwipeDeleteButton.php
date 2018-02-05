7a3efe0b974e2ad921b8fa92ce17bde2f8791ad3
Daniel Minchev <daniel.minchev@appzio.com>
2018-02-05 15:16:26 +0200

    configurable delete path for swipe to delete

--- Components/AppzioUiKit/Buttons/uiKitSwipeDeleteButton.php
+++ Components/AppzioUiKit/Buttons/uiKitSwipeDeleteButton.php
@@ -12,6 +12,11 @@ trait uiKitSwipeDeleteButton
     {
         /** @var BootstrapComponent $this */
         $identifier = $params['identifier'];
+        $delete_path = 'Controller/delete/' . $identifier;
+
+        if ( isset($params['delete_path']) AND $params['delete_path'] ) {
+        	$delete_path = $params['delete_path'];
+        }
 
         return $this->getComponentRow(array(
             $this->getComponentImage('icons8-trash.png', array(
@@ -21,8 +26,9 @@ trait uiKitSwipeDeleteButton
             'style' => 'uikit_swipe_delete_button',
             'onclick' => array(
                 $this->getOnclickHideElement('row_' . $identifier),
-                $this->getOnclickSubmit('Controller/delete/' . $identifier)
+                $this->getOnclickSubmit($delete_path)
             )
         ));
     }
+
 }
