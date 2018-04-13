82fdba95493ce0d1c20174a65ce5722138cc321f
Daniel Minchev <dogostz@gmail.com>
2018-04-13 10:30:36 +0300

    Floating navigation buttons component.

---
+++ Components/AppzioUiKit/Navigation/uiKitFloatingButtons.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Bootstrap\Components\AppzioUiKit\Navigation;
+use Bootstrap\Components\BootstrapComponent;
+
+trait uiKitFloatingButtons
+{
+
+	public function uiKitFloatingButtons(array $buttons, $overlay = true,  $parameters = [], $styles = []) {
+        /** @var BootstrapComponent $this */
+
+        if ( empty($buttons) ) {
+            return $this->uiKitDefaultHeader('{#missing_buttons#}');
+        }
+
+        $component_styles = [
+            'text-align' => 'right',
+            'margin' => '0 10 10 0'
+        ];
+
+        if ( $overlay ) {
+            $layout = new \stdClass();
+            $layout->bottom = '10';
+            $layout->height = '80';
+            $layout->right = '15';
+            $layout->width = ( count($buttons) * 80 );
+
+            $parameters['layout'] = $layout;
+        }
+
+        if ( !empty($styles) ) {
+            $component_styles = array_merge($component_styles, $styles);
+        }
+
+        return $this->getComponentRow(
+            $this->getButtons( $buttons ), $parameters, $component_styles
+        );
+    }
+
+    private function getButtons( $buttons ) {
+
+	    $data = [];
+
+        foreach ($buttons as $button) {
+
+            if ( !isset($button['icon']) OR empty($button['icon']) )
+                continue;
+
+            $parameters = [];
+
+            if ( isset($button['onclick']) AND $button['onclick'] ) {
+                $parameters['onclick'] = $button['onclick'];
+            }
+
+            $data[] = $this->getComponentImage($button['icon'], $parameters, [
+                'width' => '70',
+                'shadow-color' => '#DDE2DE',
+                'shadow-radius' => '1',
+                'shadow-offset' => '0 3',
+            ]);
+	    }
+
+	    return $data;
+    }
+
+}
