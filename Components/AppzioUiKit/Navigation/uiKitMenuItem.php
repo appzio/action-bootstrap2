04c82bc12452a4bd56aedaff9f367323a5e25596
Daniel Minchev <dogostz@gmail.com>
2018-08-08 13:37:14 +0300

    Added back_button flag as a default

--- Components/AppzioUiKit/Navigation/uiKitMenuItem.php
+++ Components/AppzioUiKit/Navigation/uiKitMenuItem.php
@@ -1,20 +1,20 @@
 <?php
 
 namespace Bootstrap\Components\AppzioUiKit\Navigation;
-use Bootstrap\Components\BootstrapComponent;
 
 trait uiKitMenuItem
 {
 
-    public function uiKitMenuItem($item = [], array $parameters = [], array $styles = []) {
+    public function uiKitMenuItem($item = [], array $parameters = [], array $styles = [])
+    {
 
-        if ( !isset($item['name']) ) {
+        if (!isset($item['name'])) {
             return $this->getComponentText('{#missing_name#}', [
                 'style' => 'article-uikit-error'
             ]);
         }
 
-        if ( isset($item['icon']) AND $item['icon'] ) {
+        if (isset($item['icon']) AND $item['icon']) {
             $menu_content[] = $this->getComponentImage($item['icon'], [
                 'style' => 'uikit_menu_item_image'
             ]);
@@ -24,33 +24,36 @@ trait uiKitMenuItem
             'style' => 'uikit_menu_item_label'
         ]);
 
-        return $this->getComponentRow($menu_content, $this->getParameters( $item, $parameters ));
+        return $this->getComponentRow($menu_content, $this->getParameters($item, $parameters));
     }
 
-    protected function getParameters( $item, $parameters ) {
+    protected function getParameters($item, $parameters)
+    {
 
-        if ( empty($parameters['style']) ) {
+        $openparams = [
+            'back_button' => 1
+        ];
+
+        if (empty($parameters['style'])) {
             $parameters['style'] = 'uikit_menu_item_row';
         }
 
-        if ( !isset($item['link']) OR empty($item['link']) ) {
+        if (!isset($item['link']) OR empty($item['link'])) {
             return $parameters;
         }
 
-        if(isset($item['tab']) AND $item['tab']){
+        if (isset($item['tab']) AND $item['tab']) {
             $openparams['tab_id'] = $item['tab'];
-        } else {
-            $openparams = array();
         }
 
-        if ( is_numeric($item['link']) ) {
-            $click['onclick'] = $this->getOnclickOpenAction(false, $item['link'],$openparams);
-        } else if ( stristr($item['link'], 'http') ) {
+        if (is_numeric($item['link'])) {
+            $click['onclick'] = $this->getOnclickOpenAction(false, $item['link'], $openparams);
+        } else if (stristr($item['link'], 'http')) {
             $click['onclick'] = $this->getOnclickOpenUrl($item['link']);
-        } else if ( $item['link'] == 'go-home' ) {
+        } else if ($item['link'] == 'go-home') {
             $click['onclick'] = $this->getOnclickGoHome();
         } else {
-            $click['onclick'] = $this->getOnclickOpenAction( $item['link'] ,$openparams);
+            $click['onclick'] = $this->getOnclickOpenAction($item['link'], $openparams);
         }
 
         return array_merge($parameters, $click);
