e3316466670f71cda0b1659b00bff99e644cd473
Daniel Minchev <dogostz@gmail.com>
2018-09-21 12:29:32 +0300

    PSR-2 standard applied.

--- Components/AppzioUiKit/Forms/uiKitGeneralField.php
+++ Components/AppzioUiKit/Forms/uiKitGeneralField.php
@@ -1,52 +1,55 @@
 <?php
 
 namespace Bootstrap\Components\AppzioUiKit\Forms;
+
 use Bootstrap\Components\BootstrapComponent as BootstrapComponent;
 
-trait uiKitGeneralField {
+trait uiKitGeneralField
+{
 
-    public function uiKitGeneralField($field,$title,$icon=false,$params=array()){
+    public function uiKitGeneralField($field, $title, $icon = false, $params = array())
+    {
         /** @var BootstrapComponent $this */
 
         $params_initial['variable'] = $field;
         $params_initial['hint'] = $title;
         $params_initial['style'] = 'uikit-general-field-text';
 
-        if($field == 'email'){
+        if ($field == 'email') {
             $params_initial['input_type'] = 'email';
-        }elseif($field == 'phone'){
+        } elseif ($field == 'phone') {
             $params_initial['input_type'] = 'number';
         }
 
         $params = array_merge($params_initial, $params);
 
-        if($icon){
-            $col[] = $this->getComponentImage($icon,array('style' => 'uikit-general-field-icon'));
+        if ($icon) {
+            $col[] = $this->getComponentImage($icon, array('style' => 'uikit-general-field-icon'));
         } else {
-            $col[] = $this->getComponentText('',array('style' => 'uikit-general-field-icon'));
+            $col[] = $this->getComponentText('', array('style' => 'uikit-general-field-icon'));
         }
 
-        if($this->model->getSubmittedVariableByName($field)){
+        if ($this->model->getSubmittedVariableByName($field)) {
             $value = $this->model->getSubmittedVariableByName($field);
-        } elseif($this->model->getSavedVariable($field)){
+        } elseif ($this->model->getSavedVariable($field)) {
             $value = $this->model->getSavedVariable($field);
         } else {
             $value = '';
         }
 
-        if(stristr($field, 'password')){
-            $col[] = $this->getComponentFormFieldPassword($this->model->getSubmittedVariableByName($field),$params);
+        if (stristr($field, 'password')) {
+            $col[] = $this->getComponentFormFieldPassword($this->model->getSubmittedVariableByName($field), $params);
         } else {
-            $col[] = $this->getComponentFormFieldText($value,$params);
+            $col[] = $this->getComponentFormFieldText($value, $params);
         }
 
-        if(isset($this->model->validation_errors[$field])){
+        if (isset($this->model->validation_errors[$field])) {
             $row[] = $this->getComponentRow($col, array(), array('vertical-align' => 'middle'));
             $row[] = $this->getComponentText($this->model->validation_errors[$field], array('style' => 'uikit-general-field-error'));
             return $this->getComponentColumn($row);
         }
 
-        if(isset($params['error']) AND $params['error']){
+        if (isset($params['error']) AND $params['error']) {
             $row[] = $this->getComponentRow($col, array(), array('vertical-align' => 'middle'));
             $row[] = $this->getComponentText($params['error'], array('style' => 'uikit-general-field-error'));
             return $this->getComponentColumn($row);
--- Components/ClientComponents/FullpageLoader.php
+++ Components/ClientComponents/FullpageLoader.php
@@ -1,9 +1,11 @@
 <?php
 
 namespace Bootstrap\Components\ClientComponents;
+
 use Bootstrap\Views\BootstrapView;
 
-trait FullpageLoader {
+trait FullpageLoader
+{
 
     /**
      * Shows a simple loader element (spinning wheel)
@@ -19,17 +21,21 @@ trait FullpageLoader {
      * @return \stdClass
      */
 
-    public function getComponentFullPageLoader(array $parameters=array(),array $styles=array()) {
+    public function getComponentFullPageLoader(array $parameters = array(), array $styles = array())
+    {
         /** @var BootstrapView $this */
 
         $color = isset($parameters['color']) ? $parameters['color'] : '#000000';
         $text = isset($parameters['text']) ? $parameters['text'] : '{#loading#}';
 
         $col[] = $this->getComponentSpacer('80');
-        $col[] = $this->getComponentLoader(array(),array('color' => $color));
-        $col[] = $this->getComponentText($text,array('style' => 'loader-text'));
+        $col[] = $this->getComponentLoader(array(), array('color' => $color));
+        $col[] = $this->getComponentText($text, array('style' => 'loader-text'));
 
-        return $this->getComponentColumn($col,array('text-align' => 'center','width' => '100%','align' => 'center'));
+        return $this->getComponentColumn($col, array(
+            'text-align' => 'center',
+            'width' => '100%'
+        ));
 
     }
 
